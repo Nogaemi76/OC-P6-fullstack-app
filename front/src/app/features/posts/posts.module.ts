49e31af015f4cc16cac95def10fecf02eb91cf9f
Emilie <milogare@yahoo.fr>
2024-10-18 01:06:08 +0200

    Added components Detail, Form, List declarations & SharedModule in Posts module, added Header in posts components

--- front/src/app/features/posts/posts.module.ts
+++ front/src/app/features/posts/posts.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PostsRoutingModule } from './posts-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { DetailComponent } from './components/detail/detail.component';
+import { FormComponent } from './components/form/form.component';
+import { ListComponent } from './components/list/list.component';
 
+const materialModules = []
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DetailComponent,
+    FormComponent,
+    ListComponent
+  ],
   imports: [
     CommonModule,
-    PostsRoutingModule
+    PostsRoutingModule,
+    SharedModule,
+    //...materialModules
   ]
 })
 export class PostsModule { }
