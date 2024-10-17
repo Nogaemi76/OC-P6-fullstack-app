3d3e8caec9022d7fda502c0e04d9784d8b9f0944
Emilie <milogare@yahoo.fr>
2024-10-18 00:57:47 +0200

    Added Header in profile

--- front/src/app/features/users/users.module.ts
+++ front/src/app/features/users/users.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
+import { ProfileComponent } from './Components/profile/profile.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
+const materialModules = []
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProfileComponent
+  ],
   imports: [
     CommonModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    SharedModule,
+    //...materialModules
   ]
 })
 export class UsersModule { }
