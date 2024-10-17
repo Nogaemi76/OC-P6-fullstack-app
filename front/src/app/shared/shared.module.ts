bbfa12096bf6fbf2a67a79286a4f281516304107
Emilie <milogare@yahoo.fr>
2024-10-18 00:56:49 +0200

    Added TopicCard in SharedComponents, & layout in Header & TopicCard

--- front/src/app/shared/shared.module.ts
+++ front/src/app/shared/shared.module.ts
@@ -3,19 +3,29 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { TopicCardComponent } from './components/topic-card/topic-card.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 const materialModules = [
-  MatToolbarModule
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule
 ];
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [
+    HeaderComponent,
+    TopicCardComponent],
   imports: [
     CommonModule,
+    FlexLayoutModule,
     ...materialModules
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    TopicCardComponent
   ]
 })
 export class SharedModule { }
