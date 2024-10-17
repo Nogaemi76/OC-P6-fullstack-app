3847e26c37b5ea8c10d311ed22d3f6ad24ecd679
Emilie <milogare@yahoo.fr>
2024-10-18 01:09:40 +0200

    Added list component declaration & modules Shared, Material & FlexLayout, & added header and layout in topic list component

--- front/src/app/features/topics/topics.module.ts
+++ front/src/app/features/topics/topics.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TopicsRoutingModule } from './topics-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ListComponent } from './Components/list/list.component';
 
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+const materialModules = [
+  MatGridListModule,
+  MatCardModule,
+  MatButtonModule,
+]
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListComponent
+  ],
   imports: [
     CommonModule,
-    TopicsRoutingModule
+    TopicsRoutingModule,
+    SharedModule,
+    FlexLayoutModule,
+    ...materialModules
   ]
 })
 export class TopicsModule { }
