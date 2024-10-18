75e941ffcd1f992919d38fe45a9dd2c008b0a732
Emilie <milogare@yahoo.fr>
2024-10-18 12:58:14 +0200

    Added form in post create

--- front/src/app/features/posts/posts.module.ts
+++ front/src/app/features/posts/posts.module.ts
@@ -4,11 +4,28 @@ import { CommonModule } from '@angular/common';
 import { PostsRoutingModule } from './posts-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
 import { DetailComponent } from './components/detail/detail.component';
 import { FormComponent } from './components/form/form.component';
 import { ListComponent } from './components/list/list.component';
 
-const materialModules = []
+const materialModules = [
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatSelectModule
+]
 
 @NgModule({
   declarations: [
@@ -20,7 +37,10 @@ const materialModules = []
     CommonModule,
     PostsRoutingModule,
     SharedModule,
-    //...materialModules
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ...materialModules
   ]
 })
 export class PostsModule { }
