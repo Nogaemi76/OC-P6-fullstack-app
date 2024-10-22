0e19a8ecbe689800d46b90aec72857163d6088b9
Emilie <milogare@yahoo.fr>
2024-10-22 21:56:57 +0200

    [FRONT A18] Added HeaderComponent content & header in LoginComponent

--- front_a18/src/app/shared/components/header/header.component.ts
+++ front_a18/src/app/shared/components/header/header.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+const materialModules = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule
+];
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterOutlet,
+    ...materialModules
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
