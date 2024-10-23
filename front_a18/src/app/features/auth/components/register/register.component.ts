90af0922457f4ae5003b59e4ef76fc8c86011586
Emilie <milogare@yahoo.fr>
2024-10-23 23:01:15 +0200

    Added content in register component & minor changes in login scss

--- front_a18/src/app/features/auth/components/register/register.component.ts
+++ front_a18/src/app/features/auth/components/register/register.component.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
 
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+import { HeaderComponent } from '../../../../shared/components/header/header.component';
+
+const materialModules = [
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule
+]
+
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterOutlet,
+    FormsModule,
+    ReactiveFormsModule,
+    ...materialModules,
+    HeaderComponent
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
