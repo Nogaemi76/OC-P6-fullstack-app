7260dc5c8c698518be28434c81ce5ee34f955501
Emilie <milogare@yahoo.fr>
2024-10-23 22:46:42 +0200

    [FRONT A18] Added content in login component

--- front_a18/src/app/features/auth/components/login/login.component.ts
+++ front_a18/src/app/features/auth/components/login/login.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+
 import { HeaderComponent } from "../../../../shared/components/header/header.component";
+
+const materialModules = [
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule
+]
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [
+    RouterLink,
+    RouterOutlet,
+    FormsModule,
+    ReactiveFormsModule,
+    ...materialModules,
+    HeaderComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
