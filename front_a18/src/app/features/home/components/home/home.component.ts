a942db0196b7e384facadcfd8db490625e1b0179
Emilie <milogare@yahoo.fr>
2024-10-25 17:01:38 +0200

    [FRONT A18] Added content in home component

--- front_a18/src/app/features/home/components/home/home.component.ts
+++ front_a18/src/app/features/home/components/home/home.component.ts
@@ -2,12 +2,19 @@ import { Component } from '@angular/core';
 
 import { RouterLink, RouterOutlet } from '@angular/router';
 
+import { MatButtonModule } from '@angular/material/button';
+
+const materialModules = [
+  MatButtonModule,
+]
+
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     RouterLink,
-    RouterOutlet
+    RouterOutlet,
+    ...materialModules,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
