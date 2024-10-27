7b16ce311bc3f98761ec9955bd5fd1da23ef5d77
Emilie <milogare@yahoo.fr>
2024-10-28 00:29:33 +0100

    [FRONT A18] Added form control to login & modified login interface

--- front_a18/src/app/features/auth/components/login/login.component.ts
+++ front_a18/src/app/features/auth/components/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { RouterLink, RouterOutlet } from '@angular/router';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -34,5 +34,14 @@ const materialModules = [
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  loginForm = new FormGroup({
+    user: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.min(8)])
+  });
+
+   handleSubmit() {
+    alert(this.loginForm.value.user + ' | ' + this.loginForm.value.password);
+   }
 
 }
