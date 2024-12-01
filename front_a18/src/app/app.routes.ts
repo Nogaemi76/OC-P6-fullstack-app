130958152b692f2b70ea9ef8e5a43c0d44795eca
Emilie <milogare@yahoo.fr>
2024-12-01 23:11:08 +0100

    [FRONT A18] Added not-found component & route

--- front_a18/src/app/app.routes.ts
+++ front_a18/src/app/app.routes.ts
@@ -1,24 +1,38 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
-
-  { path: '', component: HomeComponent },
+  {
+    path: '',
+    component: HomeComponent,
+  },
   {
     path: 'auth',
-    loadChildren: () => import('./features/auth/auth.routes').then(x => x.authRoutes)
+    loadChildren: () =>
+      import('./features/auth/auth.routes').then((x) => x.authRoutes),
   },
   {
     path: 'posts',
-    loadChildren: () => import('./features/posts/posts.routes').then(x => x.postsRoutes)
+    loadChildren: () =>
+      import('./features/posts/posts.routes').then((x) => x.postsRoutes),
   },
   {
     path: 'topics',
-    loadChildren: () => import('./features/topics/topics.routes').then(x => x.topicsRoutes)
+    loadChildren: () =>
+      import('./features/topics/topics.routes').then((x) => x.topicsRoutes),
   },
   {
     path: 'user',
-    loadChildren: () => import('./features/user/user.routes').then(x => x.userRoutes)
-  }
-
+    loadChildren: () =>
+      import('./features/user/user.routes').then((x) => x.userRoutes),
+  },
+  {
+    path: '404',
+    component: NotFoundComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+  },
 ];
