789b2f5268329ecf5ed3718cc341a129a9bc49cd
Emilie <milogare@yahoo.fr>
2024-12-02 20:48:50 +0100

    [FRONT A18] Added auth guard to routes posts, topics, user-profile

--- front_a18/src/app/app.routes.ts
+++ front_a18/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -14,16 +15,19 @@ export const routes: Routes = [
   },
   {
     path: 'posts',
+    canActivate: [authGuard],
     loadChildren: () =>
       import('./features/posts/posts.routes').then((x) => x.postsRoutes),
   },
   {
     path: 'topics',
+    canActivate: [authGuard],
     loadChildren: () =>
       import('./features/topics/topics.routes').then((x) => x.topicsRoutes),
   },
   {
     path: 'user',
+    canActivate: [authGuard],
     loadChildren: () =>
       import('./features/user/user.routes').then((x) => x.userRoutes),
   },
---
+++ front_a18/src/app/guards/auth.guard.ts
@@ -0,0 +1,17 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { UserSessionService } from '../services/user-session.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+
+  const router = inject(Router);
+  const userSessionService = inject(UserSessionService);
+
+  if(!userSessionService.isLogged) {
+    router.navigate(['auth/login']);
+    return false;
+
+  } else {
+    return true;
+  }
+};
